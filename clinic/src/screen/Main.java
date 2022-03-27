4a671ab8790c0cee6e5434e2a6b2059859713bc3
Wallace Tavares Oliveira <wallacetavaresoliveira84@gmail.com>
2022-03-27 19:51:28 -0300

    first commit

---
+++ clinic/src/screen/Main.java
@@ -0,0 +1,9 @@
+package screen;
+
+public class Main {
+
+	// All starts on "main" method
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ exercicios/src/fundamentos/FisrtsProgram.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+/**
+ * 
+ * Relevant informations
+ * @author WallaceTOliverira <wallatavaresoliveira84@gemail.com>
+ * @since 1.0
+ *
+ */
+public class FisrtsProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!!");
+	}
+}
